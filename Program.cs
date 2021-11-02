940b638874f77b40c55ef64cd3f39002203fac09
silverback23 <cahid23@hotmail.com>
2021-11-02 14:31:37 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Metotlar_Recursive
+{
+    class Program
+    {
+     public int Expo(int sayi,int üs)
+     {
+         if(üs<2)
+          return sayi;
+        return Expo(sayi,üs-1)*sayi;
+     }        
+    }
+}
